2c3801827e0a11ae80530dddf5f33d0168c1cdd6
KacperEilisz <kacpereilisz@gmail.com>
2025-05-19 20:36:35 +0200

    Add files via upload

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ 
+ @auth
+ <p>Jestes zalogowany!</p>
+ <form action="/logout" method="POST">
+  @csrf
+  <button>Wyloguj</button>
+</form>
+ @else
+<div style="border: 3px solid black;">
+        <h2>Rejestracja</h2>
+        <form action="/register" method="POST">
+            @csrf
+            <input name="name" type="text" placeholder="name">
+            <input name="email" type="text" placeholder="email">
+            <input name="password" type="password" placeholder="password">
+            <button>Zarejestruj</button>
+        </form>
+    </div>
+<div style="border: 3px solid black;">
+        <h2>Logowanie</h2>
+        <form action="/login" method="POST">
+            @csrf
+            <input name="loginname" type="text" placeholder="name">
+            <input name="loginpassword" type="password" placeholder="password">
+            <button>Zaloguj</button>
+        </form>
+    </div>
+ @endauth
+ 
+    
+</body>
+</html>
